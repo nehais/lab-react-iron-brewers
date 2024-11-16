08b4f7dd286aae0f6424a16aa307ceb3fcb35fdb
Neha Sinha <nehasinha82@gmail.com>
2024-11-16 15:39:49 +0100

    Lab done

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,22 @@
-function Search() {
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function Search({ setBeers }) {
+  const [searchStr, setSearchStr] = useState("");
+
+  useEffect(() => {
+    const searchBeers = () => {
+      axios
+        .get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${searchStr}`)
+        .then((response) => setBeers(response.data))
+        .catch((error) => console.log(error));
+    };
+
+    if (searchStr) {
+      searchBeers();
+    }
+  }, [searchStr]);
+
   return (
     <div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
       <div className="input-group mb-2 w-50">
@@ -10,6 +28,10 @@ function Search() {
         <input
           type="text"
           className="form-control search-bar"
+          value={searchStr}
+          onChange={(e) => {
+            setSearchStr(e.target.value);
+          }}
         />
       </div>
     </div>
